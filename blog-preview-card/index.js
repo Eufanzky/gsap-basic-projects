1ee3ca617299be2a1afca314919673fd8e485883
eufanzky <eufanzky@gmail.com>
2026-01-08 17:32:14 -0400

    delete comments in inde.js

--- blog-preview-card/index.js
+++ blog-preview-card/index.js
@@ -14,12 +14,6 @@ const init = () => {
       duration: 1,
       ease: "power2.out",
     });
-  // .from(".card__image", {opacity: 0, duration: 0.5, scale: 3}, "-=0.4")
-  // .from(".card__date", {opacity: 0, duration: 0.5, scale: 3}, "-=0.4")
-  // .from(".card__category", {opacity: 0, duration: 0.4, scale: 3}, "-=0.3")
-  // .from(".card__title", {opacity: 0, duration: 0.4, scale: 3}, "-=0.3")
-  // .from(".card__description", {opacity: 0, duration: 0.4, scale: 3}, "-=0.2")
-  // .from(".card__author", {opacity: 0, duration: 0.4, scale: 3}, "-=0.2")
 
   GSDevTools.create();
 };
